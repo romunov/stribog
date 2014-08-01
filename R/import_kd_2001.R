3b14537855165d83d5755b3cc78044c41843cd80
romunov <romunov@gmail.com>
2014-08-01 20:18:38 +0200

    moj brute force, ki ni obrodil sadov

--- R/import_kd_2001.R
+++ R/import_kd_2001.R
@@ -10,8 +10,15 @@ unzip(paste("./", fn, ".zip", sep = ""), exdir = "./kd2001")
 infiles <- list.files(paste("./", fn, sep = ""), full.names = TRUE)
 
 # all possible encodings 
-iconvlist() # brute force attack je razkril "WINDOWS-1250" kot najbolj primerno glede šumnikov et al
+#iconvlist() # brute force attack je razkril "WINDOWS-1250" kot najbolj primerno glede šumnikov et al
 
 kd <- read.table(infiles[grepl("KDINTKD\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250")
 kdo <- read.table(infiles[grepl("KDINTKDO\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250")
 sif <- read.table(infiles[grepl("ifranti\\.txt", infiles)], header = FALSE, sep = "$")
+
+# takole sem jaz probal na silo, pa ni vrglo ven pravega
+mm <- sapply(iconvlist(), FUN = function(x) {
+      out <- tryCatch(
+         iconv(sif, from = x, to = "UTF8"), error = function(e) "fak")
+      out[1:2]
+   })

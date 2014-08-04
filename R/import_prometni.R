6f5aa4384ddd735409c0f9fca911598fdfc0b00d
Crt Ahlin <crt.ahlin@gmail.com>
2014-08-04 12:48:40 +0200

    Added code for naming columns of traffic data.

--- R/import_prometni.R
+++ R/import_prometni.R
@@ -1,22 +1,89 @@
 # Uvoz podatkov o prometni varnosti - Traffic safety records import
 
-# generic function to import traffic data
-# takes the year as an argument
-importTrafficData <- function (leto) {
-  fileUrl <- paste0("http://policija.si/baza/pn", leto, ".zip") # construct URL of ZIP file with data
-  tempFile <- tempfile() # assign a temp file on hard drive
-  download.file(url=fileUrl, destfile=tempFile) # download the ZIP file into the temp file
-  
-  # load data into tables for OSEBE and DOGODKI
-  dataOsebe <- read.table(unz(description = tempFile, filename = "OSEBE.TXT"), encoding="WINDOWS-1250", header = FALSE, sep="$")
-  dataDogodki <- read.table(unz(description = tempFile, filename = "DOGODKI.TXT"), encoding="WINDOWS-1250", header = FALSE, sep="$")
-  unlink(tempFile) # remove temp file
-return(list(dataOsebe, dataDogodki)) # return a list with both data frames
-}
+# set working directory
+getwd
+setwd("./rawdata/prometni/pn2013")
 
 # list of years to import
-seznamLet <- c("2013","2012", "2011")
+yearsToImport <- c("2013","2012", "2011", "10", "09", "08", "07", "06", "05", "04", "03", "02", "01", "00", "99","98","97","96","95" )
 
+# read data for 2013
+leto2013 <- importTrafficData("2013")
+# rename columns
+leto2013[[1]] <- nameOsebeColumns(leto2013[[1]])
+leto2013[[2]] <- nameDogodkiColumns(leto2013[[2]]) 
+
+
+# generic function to import traffic data
+# NOTE: valid for year 2013!
+# takes the year as an argument
+importTrafficData <- function (leto) {
+  # fileUrl <- paste0("http://policija.si/baza/pn", leto, ".zip") # construct URL of ZIP file with data
+  # tempFile <- tempfile() # assign a temp file on hard drive
+  # download.file(url=fileUrl, destfile=tempFile) # download the ZIP file into the temp file
+  
+  # load data into tables for OSEBE and DOGODKI
+  # dataOsebe <- read.table(unz(description = tempFile, filename = "OSEBE.TXT"), encoding="WINDOWS-1250", header = FALSE, sep="$", colClasses="character")
+  # dataDogodki <- read.table(unz(description = tempFile, filename = "DOGODKI.TXT"), encoding="WINDOWS-1250", header = FALSE, sep="$", colClasses="character")
+  # unlink(tempFile) # remove temp file
+  
+  dataOsebe <- read.table("OSEBE.TXT",
+                          header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250", colClasses="character")
+  dataDogodki <- read.table("DOGODKI.TXT",
+                            header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250", colClasses="character")  
+  return(list(dataOsebe, dataDogodki)) # return a list with both data frames
+}
+
+# name columns os OSEBE table 
+# NOTE: valid for year 2013!
+nameOsebeColumns <- function(data) {
+  colnames(data) <- c("StevilkaZadeve",
+                      "unknown",
+                      "VlogaOsebe",
+                      "Starost",
+                      "Spol",
+                      "Obcina",
+                      "Drzavljanstvo",
+                      "Poskodba",
+                      "VrstaUdelezenca",
+                      "UporabaPasuCelade",
+                      "VozniskiStaz",
+                      "VrednostAlkotesta",
+                      "VrednostStrokovnegaPregleda",
+                      "empty"
+  )
+  return(data)
+}
+
+# name columns of DOGODKI table 
+# NOTE: valid for year 2013!
+nameDogodkiColumns <- function(data) {
+  colnames(data) <- c("StevilkaZadeve",
+                      "KlasifikacijaNesrece",
+                      "UpravnaEnotaDogodka",
+                      "DatumNesrece",
+                      "UraNesrece",
+                      "NaseljeAliIzven",
+                      "KategorijaCeste",
+                      "OznakaCestaAliNaselja?",
+                      "OznakaCestaAliNaselja?",
+                      "TekstCesteAliNaselja",
+                      "TekstOdsekaAliUlice",
+                      "StacionazaAliHisnaStevilka",
+                      "OpisPrizorisca",
+                      "GlavniVzrok",
+                      "TipNesrece",
+                      "VremenskeOkoliscine",
+                      "StanjePrometa",
+                      "StanjeVozisca",
+                      "StanjePovrsineVozisca"
+  )
+  return(data)
+}
+
+
+
+# OBSOLETE ####
 # loop to import data for several years
 # NOTE: the filenames are different through the years, so the import function will not work...
 podatki <- list()
@@ -24,6 +91,3 @@ for (leto in seznamLet) {
   podatki[[leto]] <- importTrafficData(leto)
   
 }
-
-# test reading data
-x <- importTrafficData("2013")

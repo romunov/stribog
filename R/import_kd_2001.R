7eceba4e4c8479309658be6a557630c71d0b2458
romunov <romunov@gmail.com>
2014-08-07 13:49:12 +0200

    komitam, da lahko mergam v main branch

--- R/import_kd_2001.R
+++ R/import_kd_2001.R
@@ -8,17 +8,29 @@ dir.create(fn)
 unzip(paste("./", fn, ".zip", sep = ""), exdir = "./kd2001")
 
 infiles <- list.files(paste("./", fn, sep = ""), full.names = TRUE)
-
+infiles
 # all possible encodings 
 #iconvlist() # brute force attack je razkril "WINDOWS-1250" kot najbolj primerno glede šumnikov et al
 
-kd <- read.table(infiles[grepl("KDINTKD\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250")
-kdo <- read.table(infiles[grepl("KDINTKDO\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "WINDOWS-1250")
-sif <- read.table(infiles[grepl("ifranti\\.txt", infiles)], header = FALSE, sep = "$")
+kd <- read.table(infiles[grepl("KDINTKD\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "windows-1250", encoding = "UTF8")
+kdo <- read.table(infiles[grepl("KDINTKDO\\.txt", infiles)], header = FALSE, sep = "$", fileEncoding = "windows-1250", encoding = "UTF8")
 
-# takole sem jaz probal na silo, pa ni vrglo ven pravega
-mm <- sapply(iconvlist(), FUN = function(x) {
-      out <- tryCatch(
-         iconv(sif, from = x, to = "UTF8"), error = function(e) "fak")
-      out[1:2]
-   })
+# spucaj šifrant
+sif <- readLines(infiles[grepl("ifranti\\.txt", infiles)], encoding = "WINDOWS-1250")
+
+# struktura baze, glej navodila_kriminaliteta.pdf
+names(kd) <- c(
+   "st",                 #1
+   "datum",              #2
+   "ura",                #3
+   "dan",                #4
+   "pu",                 #5
+   "povratnik",          #6
+   "klasifikacija_kd",   #7
+   "poglavje_zakonika",  #8
+   "gospodarska_k",      #9
+   "organizirana_k",     #10
+   "mladoletniška_k",    #11
+   "dokoncnost",         #12
+   ""
+   )
